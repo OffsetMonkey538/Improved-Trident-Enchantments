93dea60e761b859f63aebbabf367ae664e97ad0f
Dplayend <69760998+Dplayend@users.noreply.github.com>
2023-07-18 17:20:10 -0400

    port 1.20.x for fabric

---
+++ src/main/java/com/dplayend/ite/ImprovedTridentEnchantments.java
@@ -0,0 +1,9 @@
+package com.dplayend.ite;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ImprovedTridentEnchantments implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
